7624f501b4527a3c20780ece17c16014c945c4c7
Fasovshik9 <pip.11@bk.ru>
2023-03-27 18:59:05 +0300

    add DB

--- exemple_db/Dump20230327.sql
+++ exemple_db/Dump20230327.sql
@@ -27,12 +27,10 @@ DROP TABLE IF EXISTS `tb_authorization_staff`;
 CREATE TABLE `tb_authorization_staff` (
   `login_authorization_staff` varchar(128) NOT NULL,
   `col_full_name_staff` varchar(128) NOT NULL,
-  `id_status_level_staff` int NOT NULL,
+  `col_status_level_staff` varchar(128) NOT NULL,
   `col_password_staff` varchar(45) NOT NULL,
   PRIMARY KEY (`login_authorization_staff`),
-  UNIQUE KEY `idnew_table_UNIQUE` (`login_authorization_staff`),
-  KEY `fk_id_status_level_staff_idx` (`id_status_level_staff`),
-  CONSTRAINT `fk_id_status_level_staff` FOREIGN KEY (`id_status_level_staff`) REFERENCES `tb_status_level_staff` (`id_status_level_staff`)
+  UNIQUE KEY `idnew_table_UNIQUE` (`login_authorization_staff`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -42,8 +40,33 @@ CREATE TABLE `tb_authorization_staff` (
 
 LOCK TABLES `tb_authorization_staff` WRITE;
 /*!40000 ALTER TABLE `tb_authorization_staff` DISABLE KEYS */;
-INSERT INTO `tb_authorization_staff` VALUES ('1','admin',0,'1'),('123123','123123',2,'123123'),('123123123','321321123',1,'123123');
+INSERT INTO `tb_authorization_staff` VALUES ('1','admin','Администратор','1'),('123123','123123','Администратор','123123'),('123123123','321321123','Уволен','123123'),('qweqwe','qweqwe','Пользователь','qweqwe');
 /*!40000 ALTER TABLE `tb_authorization_staff` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_department`
+--
+
+DROP TABLE IF EXISTS `tb_department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_department` (
+  `mol_department` varchar(128) NOT NULL,
+  `col_mol_full_name` varchar(128) NOT NULL,
+  PRIMARY KEY (`mol_department`),
+  UNIQUE KEY `tb_mol_department_UNIQUE` (`mol_department`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_department`
+--
+
+LOCK TABLES `tb_department` WRITE;
+/*!40000 ALTER TABLE `tb_department` DISABLE KEYS */;
+INSERT INTO `tb_department` VALUES ('Бухгалтерия','Попов С.И.'),('КТЦ','Иванов И.И.'),('ОППР','Михайлов А.С.'),('Отдел сбыта','Васильев А.А.'),('ТАИ','Соколов Ю.В.'),('ЦТС','Петров А.С.'),('Электроцех','Петров А.А.');
+/*!40000 ALTER TABLE `tb_department` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -103,6 +126,43 @@ INSERT INTO `tb_equipment_model` VALUES (' HP TFT 19\" LP1965',6,6),(' K52JT (A5
 UNLOCK TABLES;
 
 --
+-- Table structure for table `tb_equipment_report`
+--
+
+DROP TABLE IF EXISTS `tb_equipment_report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_equipment_report` (
+  `invent_number` int NOT NULL AUTO_INCREMENT,
+  `id_col_location` int NOT NULL,
+  `col_date` date NOT NULL,
+  `mol_department` varchar(45) NOT NULL,
+  `item_number` int NOT NULL,
+  `login_authorization_staff` varchar(45) NOT NULL,
+  PRIMARY KEY (`invent_number`),
+  UNIQUE KEY `invent_number_UNIQUE` (`invent_number`),
+  KEY `fk_id_col_location_idx` (`id_col_location`),
+  KEY `fk_item_number_idx` (`item_number`),
+  KEY `fk_login_authorization_staff_idx` (`login_authorization_staff`),
+  KEY `fk_mol_department_idx` (`mol_department`),
+  CONSTRAINT `fk_id_col_location` FOREIGN KEY (`id_col_location`) REFERENCES `tb_location` (`id_col_location`) ON DELETE CASCADE,
+  CONSTRAINT `fk_item_number` FOREIGN KEY (`item_number`) REFERENCES `tb_itam_number` (`item_number`) ON DELETE CASCADE,
+  CONSTRAINT `fk_login_authorization_staff` FOREIGN KEY (`login_authorization_staff`) REFERENCES `tb_authorization_staff` (`login_authorization_staff`) ON DELETE CASCADE,
+  CONSTRAINT `fk_mol_department` FOREIGN KEY (`mol_department`) REFERENCES `tb_department` (`mol_department`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_equipment_report`
+--
+
+LOCK TABLES `tb_equipment_report` WRITE;
+/*!40000 ALTER TABLE `tb_equipment_report` DISABLE KEYS */;
+INSERT INTO `tb_equipment_report` VALUES (46,3,'2023-03-27','Отдел сбыта',101310030,'1'),(47,4,'2023-03-27','ОППР',101310020,'1');
+/*!40000 ALTER TABLE `tb_equipment_report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tb_itam_number`
 --
 
@@ -125,7 +185,7 @@ CREATE TABLE `tb_itam_number` (
 
 LOCK TABLES `tb_itam_number` WRITE;
 /*!40000 ALTER TABLE `tb_itam_number` DISABLE KEYS */;
-INSERT INTO `tb_itam_number` VALUES (101310001,'i-SENSYS LBP6000'),(101310004,'i-SENSYS LBP6000'),(101310030,'Pavilion 15-cs2019ur'),(101310020,'Phaser 3100MFP/S'),(101310025,'Phaser 3100MFP/S'),(101310012,'ProBook 430G6'),(101310018,'ProDisplay P232'),(101310029,'ProDisplay P232'),(101310009,'ThinkPad 13 Intel Core i3'),(101310019,'ThinkPad 13 Intel Core i3'),(101310021,'ThinkVision T27M061C6MAT1EU'),(101310013,'X501A-XX466H'),(101310016,'X501A-XX466H');
+INSERT INTO `tb_itam_number` VALUES (123123123,' K52JT (A52J) 15.6'),(101310001,'i-SENSYS LBP6000'),(101310004,'i-SENSYS LBP6000'),(101310030,'Pavilion 15-cs2019ur'),(101310020,'Phaser 3100MFP/S'),(101310025,'Phaser 3100MFP/S'),(101310012,'ProBook 430G6'),(101310018,'ProDisplay P232'),(101310029,'ProDisplay P232'),(101310009,'ThinkPad 13 Intel Core i3'),(101310019,'ThinkPad 13 Intel Core i3'),(101310021,'ThinkVision T27M061C6MAT1EU'),(123456789,'WorkCentre 3325DNI'),(101310013,'X501A-XX466H'),(101310016,'X501A-XX466H');
 /*!40000 ALTER TABLE `tb_itam_number` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -155,93 +215,6 @@ INSERT INTO `tb_location` VALUES (1,'Центральная котельная')
 UNLOCK TABLES;
 
 --
--- Table structure for table `tb_main`
---
-
-DROP TABLE IF EXISTS `tb_main`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tb_main` (
-  `invent_number` int NOT NULL AUTO_INCREMENT,
-  `id_col_location` int NOT NULL,
-  `col_date` date NOT NULL,
-  `mol_department` varchar(45) NOT NULL,
-  `item_number` int NOT NULL,
-  `login_authorization_staff` varchar(45) NOT NULL,
-  PRIMARY KEY (`invent_number`),
-  UNIQUE KEY `invent_number_UNIQUE` (`invent_number`),
-  KEY `fk_id_col_location_idx` (`id_col_location`),
-  KEY `fk_mol_department_idx` (`mol_department`),
-  KEY `fk_item_number_idx` (`item_number`),
-  KEY `fk_login_authorization_staff_idx` (`login_authorization_staff`),
-  CONSTRAINT `fk_id_col_location` FOREIGN KEY (`id_col_location`) REFERENCES `tb_location` (`id_col_location`) ON DELETE CASCADE,
-  CONSTRAINT `fk_item_number` FOREIGN KEY (`item_number`) REFERENCES `tb_itam_number` (`item_number`) ON DELETE CASCADE,
-  CONSTRAINT `fk_login_authorization_staff` FOREIGN KEY (`login_authorization_staff`) REFERENCES `tb_authorization_staff` (`login_authorization_staff`) ON DELETE CASCADE,
-  CONSTRAINT `fk_mol_department` FOREIGN KEY (`mol_department`) REFERENCES `tb_mol` (`mol_department`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_main`
---
-
-LOCK TABLES `tb_main` WRITE;
-/*!40000 ALTER TABLE `tb_main` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tb_main` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tb_mol`
---
-
-DROP TABLE IF EXISTS `tb_mol`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tb_mol` (
-  `mol_department` varchar(128) NOT NULL,
-  `col_mol_full_name` varchar(128) NOT NULL,
-  PRIMARY KEY (`mol_department`),
-  UNIQUE KEY `tb_mol_department_UNIQUE` (`mol_department`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_mol`
---
-
-LOCK TABLES `tb_mol` WRITE;
-/*!40000 ALTER TABLE `tb_mol` DISABLE KEYS */;
-INSERT INTO `tb_mol` VALUES ('Бухгалтерия','Попов С.И.'),('КТЦ','Иванов И.И.'),('ОППР','Михайлов А.С.'),('Отдел сбыта','Васильев А.А.'),('ТАИ','Соколов Ю.В.'),('ЦТС','Петров А.С.'),('Электроцех','Петров А.А.');
-/*!40000 ALTER TABLE `tb_mol` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tb_status_level_staff`
---
-
-DROP TABLE IF EXISTS `tb_status_level_staff`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tb_status_level_staff` (
-  `id_status_level_staff` int NOT NULL,
-  `col_status_staff_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_status_level_staff`),
-  UNIQUE KEY `id_privilege_level_staff_UNIQUE` (`id_status_level_staff`),
-  UNIQUE KEY `col_privilege_staff_name_UNIQUE` (`col_status_staff_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_status_level_staff`
---
-
-LOCK TABLES `tb_status_level_staff` WRITE;
-/*!40000 ALTER TABLE `tb_status_level_staff` DISABLE KEYS */;
-INSERT INTO `tb_status_level_staff` VALUES (0,'admin'),(1,'user'),(2,'Уволен');
-/*!40000 ALTER TABLE `tb_status_level_staff` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `tb_type_equipment`
 --
 
@@ -266,14 +239,6 @@ LOCK TABLES `tb_type_equipment` WRITE;
 INSERT INTO `tb_type_equipment` VALUES (8,'веб-камера'),(9,'видеокамера'),(5,'компьютер'),(6,'монитор'),(1,'МФУ'),(7,'ноутбук'),(4,'сканер'),(2,'телефон');
 /*!40000 ALTER TABLE `tb_type_equipment` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'inventory'
---
-
---
--- Dumping routines for database 'inventory'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -284,4 +249,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-26 19:59:49
+-- Dump completed on 2023-03-27 18:58:22
